a5783ba7cfb73e3693d9c682eb7cb21486bbc7cb
Michael Aerni <michael.aerni@hotmail.com>
2017-04-05 15:58:03 +0200

    Created python project setup

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+# Load readme
+with open("README.md") as readme_file:
+    readme_text = readme_file.read()
+
+# Load license
+with open("LICENSE") as license_file:
+    license_text = license_file.read()
+
+# Load requirements
+# TODO: Best practice would be to have them separate here but we just use the pip frozen dependencies
+with open('requirements.txt') as requirements_file:
+    requirements = requirements_file.read().splitlines()
+
+setup(
+    name="molanet",
+    version="0.0.1",
+    description="Molanet skin segmentation network",
+    long_description=readme_text,
+    author="Michael Aerni, Patrick Del Conte",
+    license=license_text,
+    packages=find_packages(exclude=("tests", "docs", "notebooks")),
+    install_requires=requirements
+)
