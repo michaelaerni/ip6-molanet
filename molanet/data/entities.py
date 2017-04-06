9dc37db5ee746e3ef4cc0a1c62787f12e4a7a4ef
Michael Aerni <michael.aerni@hotmail.com>
2017-04-06 20:38:44 +0200

    Fixed first data model

--- molanet/data/entities.py
+++ molanet/data/entities.py
@@ -1,6 +1,6 @@
 from enum import Enum
 
-from bson import Binary
+from bson.binary import Binary
 from typing import Dict, Any
 
 # TODO: This is probably the least idiomatic python code ever
@@ -12,50 +12,6 @@ def entity(cls, prefix=""):
 
     cls.dict = fun
     return cls
-
-
-@entity
-class MoleSample(object):
-
-    def __init__(
-            self,
-            uuid: str,
-            data_source: str,
-            data_set: str,
-            source_id: str,
-            set_id: str,
-            dimensions: (int, int),
-            diagnosis: Diagnosis,
-            use_case: UseCase,
-            image: Binary,
-            segmentations: [Segmentation]):
-        self.uuid = uuid
-        self.data_source = data_source
-        self.data_set = data_set
-        self.source_id = source_id
-        self.set_id = set_id
-        self.dimensions = dimensions
-        self.diagnosis = diagnosis
-        self.use_case = use_case
-        self.image = image
-        self.segmentations = segmentations
-
-
-@entity
-class Segmentation(object):
-
-    def __init__(
-            self,
-            id: str,
-            mask: Binary,
-            skill_level: SkillLevel,
-            dimensions: (int, int) = None,
-            use_case = UseCase):
-        self.id = id
-        self.mask = mask,
-        self.skill_level = skill_level,
-        self.dimensions = dimensions,
-        self.use_case = use_case
 
 
 class Diagnosis(Enum):
@@ -70,7 +26,52 @@ class SkillLevel(Enum):
 
 
 class UseCase(Enum):
-    IGNORE = 0
+    UNSPECIFIED = 0
     TRAINING = 1
     TEST = 2
-    VALIDATION = 3
+    VALIDATION = 3,
+    IGNORE = 4
+
+
+@entity
+class Segmentation(object):
+
+    def __init__(
+            self,
+            id: str,
+            mask: Binary,
+            skill_level: SkillLevel,
+            dimensions: (int, int) = None,
+            use_case: UseCase = UseCase.UNSPECIFIED):
+        self.id = id
+        self.mask = mask,
+        self.skill_level = skill_level,
+        self.dimensions = dimensions,
+        self.use_case = use_case
+
+
+@entity
+class MoleSample(object):
+
+    def __init__(
+            self,
+            uuid: str,
+            data_source: str,
+            data_set: str,
+            source_id: str,
+            set_id: str,
+            dimensions: (int, int),
+            diagnosis: Diagnosis,
+            image: Binary,
+            segmentations: [Segmentation],
+            use_case: UseCase = UseCase.UNSPECIFIED,):
+        self.uuid = uuid
+        self.data_source = data_source
+        self.data_set = data_set
+        self.source_id = source_id
+        self.set_id = set_id
+        self.dimensions = dimensions
+        self.diagnosis = diagnosis
+        self.use_case = use_case
+        self.image = image
+        self.segmentations = segmentations
