8a5bc777b4f7da4f773d325a05aa91ebe628ed37
Michael Aerni <michael.aerni@hotmail.com>
2017-04-07 16:25:26 +0200

    Made names more descriptive, prevented shadowing and changed function parameter order to make sense

--- molanet/data/database.py
+++ molanet/data/database.py
@@ -5,19 +5,22 @@ from molanet.data.entities import MoleSample
 
 
 class MongoConnection(object):
-    def __init__(self, user: str, pwd: str, url: str = "mongodb://localhost:27017/", db_name: str = "molanet"):
+
+    def __init__(self, url: str = "mongodb://localhost:27017/", db_name: str = "molanet", username: str = None, password: str = None):
         self.client = MongoClient(url)
         self.database = self.client[db_name]
-        self.database.authenticate(user, pwd, mechanism='SCRAM-SHA-1')
+
+        if username is not None and password is not None:
+            self.database.authenticate(username, password)
 
     def load_data_set(self, data: [MoleSample]) -> [ObjectId]:
-        def dict_with_id(sample: MoleSample):
-            dict = sample.dict()
-            dict['_id'] = sample.uuid
-            return dict
+        def apply_id(sample: MoleSample):
+            sample_dict = sample.dict()
+            sample_dict['_id'] = sample.uuid
+            return sample_dict
 
-        dict_list = [dict_with_id(sample) for sample in data]
-        return self.database.samples.insert_many(dict_list)
+        dict_list = [apply_id(sample) for sample in data]
+        return self.database.samples.insert_many(dict_list).inserted_ids
 
     def clear_data(self, data_source: str, data_set: str = None) -> int:
         filter_dict = {"data_source": data_source}
