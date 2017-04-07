25c1d4ab5161345ed65374252b08632e6a951066
Patrick Del Conte <p.del.conte@hotmail.ch>
2017-04-07 17:03:58 +0200

    save images as numpy arrays serialized to bytes

--- molanet/data/load_isic.py
+++ molanet/data/load_isic.py
@@ -1,7 +1,10 @@
+import io
 import json
 import sys
 
+import numpy as np
 import requests
+from PIL import Image
 from bson import Binary
 
 from molanet.data.database import MongoConnection
@@ -14,27 +17,47 @@ imagesListUrl = r"https://isic-archive.com:443/api/v1/image"
 imagesListParams = {"limit": "50", "offset": "0", "sort": "name", "sortdir": "1"}
 imageDetailsUrl = "https://isic-archive.com:443/api/v1/image/"
 
-def addParam(param, key, value):
+
+def addparam(param, key, value):
     param[key] = str(value)
     return param
 
-def segmentationParamFromID(id):
-    return addParam(segmentationParams, "imageId", id)
 
-
-def list_images(numberOfImages: int, offset: int):
-    p = addParam(imagesListParams, "limit", numberOfImages)
-    r = requests.get(url=imagesListUrl, params=addParam(p, 'offset', str(offset)))
+def list_images(maxnumberofimages: int, offset: int):
+    p = addparam(imagesListParams, "limit", maxnumberofimages)
+    r = requests.get(url=imagesListUrl, params=addparam(p, 'offset', str(offset)))
     data = json.loads(r.text)
     return data
 
-def getImagesDetails(id):
+
+def getimagedetails(id):
     r = requests.get(url=imageDetailsUrl + id)
     return json.loads(r.text)
 
-def getSegmentationInfo(id):
-    r = requests.get(url=segmentationurl, params=segmentationParamFromID(id))
+
+def getsegmentationinfo(id):
+    r = requests.get(url=segmentationurl, params=addparam(segmentationParams, "imageId", id))
     return json.loads(r.text)
+
+
+def downloadbinary_isic_image(id):
+    url = 'https://isic-archive.com:443/api/v1/image/' + id + '/download'
+    return requests.get(url).content
+
+
+def downloadbinary_isic_superpixels(id):
+    url = 'https://isic-archive.com:443/api/v1/image/' + id + '/superpixels'
+    return requests.get(url).content
+
+
+def downloadbinary_isic_segmentation(segmentation_id):
+    url = 'https://isic-archive.com:443/api/v1/segmentation/' + segmentation_id + '/mask?contentDisposition=inline'
+    return requests.get(url).content
+
+
+def convert_binaryimage_numpyarray(im_binary: bytes):
+    image = Image.open(io.BytesIO(im_binary))
+    return np.array(image)
 
 
 _uuid = 'uuid'
@@ -46,49 +69,42 @@ _dimensionsX = 'dimensionsX'
 _dimensionsY = 'dimensionsY'
 _benign_malignant = 'benign_malignant'
 _diagnosis = 'diagnosis'
-_imageDownloadURl = 'imageDownloadURl'
 _imageSuperPixelsDownloadUrl = 'imageSuperPixelsDownloadUrl'
 _dataSetUniqueID = 'dataSetUniqueID'
 _segmentation = 'segmentation'
 
 
-def getImageInfo(id, resultDict={}, debug=False):
-    data = getImagesDetails(id)
-    resultDict[_dataSource] = 'isic'
-    resultDict[_uuid] = resultDict[_dataSource] + id
-    resultDict[_datasetName] = data['dataset']['name']
-    resultDict[_dataSetUniqueID] = data['dataset']['_id']
-    resultDict[_idInDataSet] = id
-    resultDict[_name] = data['name']
-    resultDict[_dimensionsX] = data['meta']['acquisition']['pixelsX']
-    resultDict[_dimensionsY] = data['meta']['acquisition']['pixelsY']
-    resultDict[_benign_malignant] = data['meta']['clinical']['benign_malignant']
-    resultDict[_diagnosis] = data['meta']['clinical']['diagnosis']
-    resultDict[_imageDownloadURl] = 'https://isic-archive.com:443/api/v1/image/' + id + '/download'
-    resultDict[_imageSuperPixelsDownloadUrl] = 'https://isic-archive.com:443/api/v1/image/' + id + '/superpixels'
+def getimageinfo(im_id, debug=False):
+    resultdict = {}
+    data = getimagedetails(im_id)
+    resultdict[_dataSource] = 'isic'
+    resultdict[_uuid] = resultdict[_dataSource] + im_id
+    resultdict[_datasetName] = data['dataset']['name']
+    resultdict[_dataSetUniqueID] = data['dataset']['_id']
+    resultdict[_idInDataSet] = im_id
+    resultdict[_name] = data['name']
+    resultdict[_dimensionsX] = data['meta']['acquisition']['pixelsX']
+    resultdict[_dimensionsY] = data['meta']['acquisition']['pixelsY']
+    resultdict[_benign_malignant] = data['meta']['clinical']['benign_malignant']
+    resultdict[_diagnosis] = data['meta']['clinical']['diagnosis']
 
-
-    segmentations = getSegmentationInfo(id)
-    resultDict[_segmentation] = segmentations
-    for seg in segmentations:
-        seg['segmentationDownloadUrl'] = \
-            'https://isic-archive.com:443/api/v1/segmentation/' + seg['_id'] + '/mask?contentDisposition=inline'
-
+    segmentations = getsegmentationinfo(im_id)
+    resultdict[_segmentation] = segmentations
     if debug:
-        print(resultDict)
+        print(resultdict)
         # save stuff to file or something
-        f = open('segmentation' + resultDict[_segmentation][0]['_id'] + '.jpg', 'wb')
-        f.write(requests.get(resultDict[_segmentation][0]['segmentationDownloadUrl']).content)
+        f = open('segmentation' + resultdict[_segmentation][0]['_id'] + '.jpg', 'wb')
+        f.write(downloadbinary_isic_segmentation(resultdict[_segmentation][0]['_id']))
         f.close()
         # save to image to a file or something
-        f = open(resultDict[_name] + '.jpg', 'wb')
-        f.write(requests.get(resultDict[_imageDownloadURl]).content)
+        f = open(resultdict[_name] + '.jpg', 'wb')
+        f.write(downloadbinary_isic_image(resultdict[_idInDataSet]))
         f.close()
-        f = open(resultDict[_name] + '_data.json', 'w')
-        f.write(json.dumps(resultDict))
+        f = open(resultdict[_name] + '_data.json', 'w')
+        f.write(json.dumps(resultdict))
         f.close()
 
-    return resultDict
+    return resultdict
 
 
 def connect(url, port, user, pw, dbname):
@@ -101,6 +117,7 @@ def parsedbconnectioninfo_connect(file):
     params = json.loads(f.read())
     return connect(params['url'], params['port'], params['user'], params['pass'], 'molanet')
 
+
 def parse_diagnosis(diagnosis):
     switch = {
         'benign': Diagnosis.BENIGN,
@@ -109,42 +126,45 @@ def parse_diagnosis(diagnosis):
     return switch.get(diagnosis, Diagnosis.UNKNOWN)
 
 
-def parse_segmentation(segmentations, dimensions):
-    def parseSkill(skill):
+def parse_segmentation(segmentations, dimensions: (int, int)):
+    def parseskill(skill):
         switch = {
             'novice': SkillLevel.NOVICE,
             'expert': SkillLevel.EXPERT
         }
         return switch[skill]
+
     parsed = []
     for seg in segmentations:
         parsed.append(Segmentation(seg['_id'],
-                                   Binary(requests.get(seg['segmentationDownloadUrl']).content),
-                                   parseSkill(seg['skill']),
+                                   Binary(np.ndarray.tobytes(convert_binaryimage_numpyarray(
+                                       downloadbinary_isic_segmentation(seg['_id'])))),
+                                   parseskill(seg['skill']),
                                    dimensions))
     return parsed
 
 
-def parsedata(dict):
-    dim = (dict[_dimensionsX], dict[_dimensionsY])
-    return MoleSample(dict[_uuid],
-                      dict[_dataSource],
-                      dict[_datasetName],
-                      dict[_idInDataSet],
-                      dict[_name],
+def parsedata(data):
+    dim = (data[_dimensionsX], data[_dimensionsY])
+    return MoleSample(data[_uuid],
+                      data[_dataSource],
+                      data[_datasetName],
+                      data[_idInDataSet],
+                      data[_name],
                       dim,
-                      Diagnosis(parse_diagnosis(dict[_benign_malignant])),
-                      Binary(requests.get(dict[_imageDownloadURl]).content),
-                      parse_segmentation(dict[_segmentation], dimensions=None))
+                      Diagnosis(parse_diagnosis(data[_benign_malignant])),
+                      Binary(np.ndarray.tobytes(
+                          convert_binaryimage_numpyarray(downloadbinary_isic_image('5436e3abbae478396759f0cf')))),
+                      parse_segmentation(data[_segmentation], dimensions=None))
 
 
-def load_isic(maxImages=15000, offset=0):
+def load_isic(maximages=15000, offset=0):
     dbconnection = parsedbconnectioninfo_connect('dbconnection.json')
     # dbconnection = MongoConnection(url='mongodb://localhost:27017/', db_name='molanet')
-    data = list_images(maxImages, offset)
+    data = list_images(maximages, offset)
     count = offset
     for imageData in data:
-        sample = parsedata(getImageInfo(imageData['_id']))
+        sample = parsedata(getimageinfo(imageData['_id']))
         from pymongo.errors import BulkWriteError
         try:
             dbconnection.load_data_set([sample])
@@ -159,5 +179,9 @@ def load_isic(maxImages=15000, offset=0):
 
 
 # p = parsedata(getImageInfo("5436e3abbae478396759f0cf",debug=True))
+# im = convert_binaryimage_numpyarray(downloadbinary_isic_image('5436e3abbae478396759f0cf'))
+# print(im.shape)
+# seg = convert_binaryimage_numpyarray(downloadbinary_isic_segmentation('5463934bbae47821f88025ad'))
+# print(seg.shape)
 # load_isic(maxImages=5)
 load_isic()
