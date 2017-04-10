8c20dff8bc6bc936addbd387466062c458242b64
Patrick Del Conte <p.del.conte@hotmail.ch>
2017-04-10 12:09:18 +0200

    support to dump images to file

--- molanet/data/load_isic.py
+++ molanet/data/load_isic.py
@@ -1,11 +1,13 @@
 import io
 import json
 import sys
+import os
 
 import numpy as np
 import requests
 from PIL import Image
-from bson import Binary
+from bson.binary import Binary
+from pymongo.errors import DocumentTooLarge
 
 from molanet.data.database import MongoConnection
 from molanet.data.entities import MoleSample, Diagnosis, Segmentation, SkillLevel
@@ -135,36 +137,54 @@ def parse_segmentation(segmentations, dimensions: (int, int)):
         return switch[skill]
 
     parsed = []
+    images = {}
     for seg in segmentations:
+        segmentation_image = downloadbinary_isic_segmentation(seg['_id'])
+        segmentation_np_image = convert_binaryimage_numpyarray(segmentation_image)
         parsed.append(Segmentation(seg['_id'],
-                                   Binary(np.ndarray.tobytes(convert_binaryimage_numpyarray(
-                                       downloadbinary_isic_segmentation(seg['_id'])))),
+                                   Binary(np.ndarray.tobytes(segmentation_np_image)),
                                    parseskill(seg['skill']),
                                    dimensions))
-    return parsed
+        images[seg['_id']] = (segmentation_image, segmentation_np_image)
+    return parsed, images
 
 
 def parsedata(data):
     dim = (data[_dimensionsX], data[_dimensionsY])
-    return MoleSample(data[_uuid],
-                      data[_dataSource],
-                      data[_datasetName],
-                      data[_idInDataSet],
-                      data[_name],
-                      dim,
-                      Diagnosis(parse_diagnosis(data[_benign_malignant])),
-                      Binary(np.ndarray.tobytes(
-                          convert_binaryimage_numpyarray(downloadbinary_isic_image('5436e3abbae478396759f0cf')))),
-                      parse_segmentation(data[_segmentation], dimensions=None))
+    image = downloadbinary_isic_image('5436e3abbae478396759f0cf')
+    np_image = convert_binaryimage_numpyarray(image)
+    (segmentations, segmentations_raw) = parse_segmentation(data[_segmentation], dimensions=None)
+    return (MoleSample(data[_uuid],
+                       data[_dataSource],
+                       data[_datasetName],
+                       data[_idInDataSet],
+                       data[_name],
+                       dim,
+                       Diagnosis(parse_diagnosis(data[_benign_malignant])),
+                       Binary(np.ndarray.tobytes(np_image)),
+                       segmentations), (image, np_image), segmentations_raw)
 
 
-def load_isic(maximages=15000, offset=0):
+def load_isic(maximages=15000, offset=0, logFile=None, images_dir=None, npimagesDir=None, seg_dir=None,
+              np_seg_dir=None):
     dbconnection = parsedbconnectioninfo_connect('dbconnection.json')
     # dbconnection = MongoConnection(url='mongodb://localhost:27017/', db_name='molanet')
     data = list_images(maximages, offset)
     count = offset
     for imageData in data:
-        sample = parsedata(getimageinfo(imageData['_id']))
+        sample, (image, np_image), seg_raw = parsedata(getimageinfo(imageData['_id']))
+
+        if (images_dir is not None):
+            with open(os.path.join(images_dir, sample.uuid + '.jpg'), 'wb') as f:
+                f.write(image)
+        if (npimagesDir is not None):
+            np.save(os.path.join(npimagesDir, sample.uuid), np_image)
+        if (seg_dir is not None and np_seg_dir is not None):
+            for seg_id, (seg, np_seg) in seg_raw.items():
+                with open(os.path.join(seg_dir, sample.uuid + seg_id + '.jpg'), 'wb') as f:
+                    f.write(seg)
+                np.save(os.path.join(np_seg_dir, sample.uuid + seg_id), np_seg)
+
         from pymongo.errors import BulkWriteError
         try:
             dbconnection.load_data_set([sample])
@@ -172,6 +192,14 @@ def load_isic(maximages=15000, offset=0):
             exc_info = sys.exc_info()
             print(exc_info)
             print("probably attempted to overwrite existing document _id=%s (aka. %s)" % (sample.uuid, sample.name))
+            pass
+        except DocumentTooLarge:
+            exc_info = sys.exc_info()
+            print(exc_info)
+            print("too large document _id=%s (aka. %s)" % (sample.uuid, sample.name))
+            if logFile is not None:
+                logFile.write('%d Document to large _id=%s (aka. %s)' % (count, sample.uuid, sample.name))
+                logFile.flush()
             pass
         # dosomething with it
         print("%d : %s from dataset %s done" % (count, sample.name, sample.data_set))
@@ -184,4 +212,30 @@ def load_isic(maximages=15000, offset=0):
 # seg = convert_binaryimage_numpyarray(downloadbinary_isic_segmentation('5463934bbae47821f88025ad'))
 # print(seg.shape)
 # load_isic(maxImages=5)
-load_isic()
+def mk_dirs_if_not_exists(rootfolder_path='samples'):
+    images = os.path.join(rootfolder_path, 'images')
+    segmentations = os.path.join(rootfolder_path, 'segmentations')
+    images_numpy = os.path.join(rootfolder_path, 'images_numpy')
+    segmentations_numpy = os.path.join(rootfolder_path, 'segmentations_numpy')
+
+    if not os.path.exists(rootfolder_path):
+        os.makedirs(rootfolder_path)
+    if not os.path.exists(images):
+        os.makedirs(images)
+    if not os.path.exists(segmentations):
+        os.makedirs(segmentations)
+    if not os.path.exists(images_numpy):
+        os.makedirs(images_numpy)
+    if not os.path.exists(segmentations_numpy):
+        os.makedirs(segmentations_numpy)
+
+    return (images, segmentations, images_numpy, segmentations_numpy)
+
+
+(images, segmentations, images_numpy, segmentations_numpy) = mk_dirs_if_not_exists('samples')
+logFilePath = "log_load_isic.txt"
+with open(logFilePath, 'w') as log:
+    load_isic(offset=377, logFile=log, images_dir=images, npimagesDir=images_numpy, seg_dir=segmentations,
+              np_seg_dir=segmentations_numpy)
+
+mk_dirs_if_not_exists()
