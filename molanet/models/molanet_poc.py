a044b193a42fe7d36b9bd9d505d9d404ac3fb49c
Michael Aerni <michael.aerni@hotmail.com>
2017-06-07 12:55:40 +0200

    Restructured PoC network to have trainable memory sizes

--- molanet/models/molanet_poc.py
+++ molanet/models/molanet_poc.py
@@ -1,4 +1,5 @@
 import tensorflow as tf
+import math
 from PIL import Image
 import numpy as np
 
@@ -26,7 +27,7 @@ def conv2d(features, feature_count, name, use_batchnorm=True, strides=None, filt
 
     w = weight_variable("w_" + name, [filter_sizes[0], filter_sizes[1], input_channels, feature_count])
     b = bias_variable("b_" + name, [feature_count])
-    conv = tf.nn.bias_add(tf.nn.conv2d(features, w, strides=[1, strides[0], strides[1], 1], padding=padding), b)
+    conv = tf.nn.bias_add(tf.nn.conv2d(features, w, strides=[1, strides[0], strides[1], 1], padding=padding, name=f"conv_{name}"), b)
     if use_batchnorm:
         bn = tf.contrib.layers.batch_norm(conv, decay=0.9, epsilon=1e-5)  # TODO: Params?
     else:
@@ -48,7 +49,7 @@ def conv2d_transpose(features, feature_count, output_size, name, keep_prob, batc
 
     w = weight_variable("w_" + name, [filter_sizes[0], filter_sizes[1], feature_count, features.get_shape()[-1]])
     b = bias_variable("b_" + name, [feature_count])
-    conv = tf.nn.bias_add(tf.nn.conv2d_transpose(features, w, output_shape=[batch_size, output_size[0], output_size[1], feature_count], strides=[1, strides[0], strides[1], 1], padding=padding), b)
+    conv = tf.nn.bias_add(tf.nn.conv2d_transpose(features, w, output_shape=[batch_size, output_size[0], output_size[1], feature_count], strides=[1, strides[0], strides[1], 1], padding=padding, name=f"conv_trans_{name}"), b)
     if use_batchnorm:
         bn = tf.contrib.layers.batch_norm(conv, decay=0.9, epsilon=1e-5)  # TODO: Params?
     else:
@@ -86,19 +87,20 @@ class MolanetPoc(object):
 
             a_enc1, _, _ = conv2d(input_tensor, 64, "enc_1", use_batchnorm=False, padding="VALID", input_channels=3)
             a_enc2, _, _ = conv2d(a_enc1, 128, "enc_2", padding="VALID")
-            a_enc3, _, _ = conv2d(a_enc2, 256, "enc_3", padding="VALID")
-            a_enc4, _, _ = conv2d(a_enc3, 256, "enc_4", padding="VALID")
-            a_enc5, _, _ = conv2d(a_enc4, 256, "enc_5", padding="VALID")
+            a_enc3, _, _ = conv2d(a_enc2, 128, "enc_3", padding="VALID")
+            a_enc4, _, _ = conv2d(a_enc3, 128, "enc_4", padding="VALID")
+            a_enc5, _, _ = conv2d(a_enc4, 128, "enc_5", padding="VALID")
 
-            a_dec1, _, _ = conv2d_transpose(a_enc5, 256, [4, 4], "dec_1", keep_prob, batch_size, concat_activations=a_enc4, use_batchnorm=False, padding="VALID")
-            a_dec2, _, _ = conv2d_transpose(a_dec1, 256, [10, 10], "dec_2", keep_prob, batch_size, concat_activations=a_enc3, padding="VALID")
+            a_dec1, _, _ = conv2d_transpose(a_enc5, 128, [4, 4], "dec_1", keep_prob, batch_size, concat_activations=a_enc4, use_batchnorm=False, padding="VALID")
+            a_dec2, _, _ = conv2d_transpose(a_dec1, 128, [10, 10], "dec_2", keep_prob, batch_size, concat_activations=a_enc3, padding="VALID")
             a_dec3, _, _ = conv2d_transpose(a_dec2, 128, [22, 22], "dec_3", keep_prob, batch_size, concat_activations=a_enc2, padding="VALID")
             a_dec4, _, _ = conv2d_transpose(a_dec3, 64, [46, 46], "dec_4", 1.0, batch_size, concat_activations=a_enc1, padding="VALID")
             a_dec5, _, _ = conv2d_transpose(a_dec4, 1, [94, 94], "dec_5", 1.0, batch_size, concat_activations=input_tensor, padding="VALID")
 
-            logits, _, _ = conv2d(a_dec5, 1, "dec_out", strides=[1, 1], filter_sizes=[25, 25], use_batchnorm=False, do_activation=False, padding="VALID", input_channels=4)
+            logits, _, _ = conv2d(a_dec5, 1, "dec_out", strides=[1, 1], filter_sizes=[5, 5], use_batchnorm=False, do_activation=False, padding="VALID", input_channels=4)
+            logits = tf.slice(logits, [0, 10, 10, 0], [batch_size, 70, 70, 1], name="crop")
 
-            # TODO: Might not work
+            # TODO: Does not work
             if reshape:
                 logits = tf.reshape(logits, [1, 11 * 70, 15 * 70, 1]) # TODO
 
@@ -114,23 +116,26 @@ class MolanetPoc(object):
             # Patch size: 70
             input_tensor = tf.concat([image, mask], axis=3)
 
-            # 70x70x4 -> 64x64x64
-            a1, w1, b1 = conv2d(input_tensor, 64, "disc_1", strides=[1, 1], filter_sizes=[7, 7], use_batchnorm=False, padding="VALID", input_channels=4)
+            # 70x70x4 -> 68x68x64
+            a1_1, w1_1, b1_1 = conv2d(input_tensor, 64, "disc_1_1", strides=[1, 1], filter_sizes=[3, 3], use_batchnorm=False, padding="VALID", input_channels=4)
+
+            # 68x68x64 -> 64x64x64
+            a1_2, w1_2, b1_2 = conv2d(a1_1, 64, "disc_1_2", strides=[1, 1], filter_sizes=[5, 5], padding="VALID")
 
             # 64x64x64 -> 32x32x128
-            a2, w2, b2 = conv2d(a1, 128, "disc_2")
+            a2, w2, b2 = conv2d(a1_2, 128, "disc_2")
 
             # 32x32x128 -> 16x16x256
             a3, w3, b3 = conv2d(a2, 256, "disc_3")
 
             # 16x16x256 -> 8x8x512
-            a4, w4, b4 = conv2d(a3, 512, "disc_4")
+            a4, w4, b4 = conv2d(a3, 256, "disc_4")
 
             # 8x8x512 -> 4x4x512
-            a5, w5, b5 = conv2d(a4, 512, "disc_5")
+            a5, w5, b5 = conv2d(a4, 256, "disc_5")
 
             # 4x4x512 -> 2x2x512
-            a6, w6, b6 = conv2d(a5, 512, "disc_6")
+            a6, w6, b6 = conv2d(a5, 256, "disc_6")
 
             # 2x2x512 -> 1x1x1
             logits, w_out, b_out = conv2d(a6, 1, "disc_out", use_batchnorm=False, do_activation=False)
@@ -201,22 +206,20 @@ if __name__ == "__main__":
     in_segmentation = (in_segmentation / 255.0 - 0.5) * 2.0
     in_segmentation = np.reshape(in_segmentation, (1, in_segmentation.shape[0], in_segmentation.shape[1], 1))
 
-    # image = tf.placeholder(tf.float32, [1, None, None, 3], name="image")
     image = tf.placeholder(tf.float32, [None, 94, 94, 3], name="image")
     image_patches = tf.placeholder(tf.float32, [None, 70, 70, 3], name="image_patches")
     mask_patches = tf.placeholder(tf.float32, [None, 70, 70, 1], name="mask_patches")
 
-    # padded_image = tf.reshape(padded_image, [1, 794, 1074, 3]) # TODO: Kill me
-    # print(padded_image.get_shape(), image.get_shape(), mask.get_shape())
-
     disc_real_net, disc_real_logits = MolanetPoc._create_discriminator(image_patches, mask_patches)
     gen_net, _ = MolanetPoc._create_generator(image)
     gen_net_use, _ = MolanetPoc._create_generator(image, reuse=True, reshape=True)
-    # gen_net = tf.reshape(gen_net, [1, 767, 1022, 1]) # TODO: Kill me
 
     disc_fake_net, disc_fake_logits = MolanetPoc._create_discriminator(image_patches, gen_net, reuse=True)
 
-    loss_gen = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(logits=disc_fake_logits, labels=tf.ones_like(disc_fake_logits)))
+    l1_lambda = 0.001
+    l1_loss = tf.reduce_sum(tf.abs((tf.round((gen_net + 1.0) / 2.0) - 0.5) * 2.0 - mask_patches))
+
+    loss_gen = l1_lambda * l1_loss + tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(logits=disc_fake_logits, labels=tf.ones_like(disc_fake_logits)))
     loss_disc_real = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(logits=disc_real_logits, labels=tf.ones_like(disc_real_logits)))
     loss_disc_fake = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(logits=disc_fake_logits, labels=tf.zeros_like(disc_fake_logits)))
     loss_disc = loss_disc_real + loss_disc_fake
@@ -247,19 +250,37 @@ if __name__ == "__main__":
         Image.fromarray(np.reshape((patches_segmentation[20] + 1.0) / 2.0 * 255.0, (patches_segmentation.shape[1], patches_segmentation.shape[2])).astype(np.uint8)).save("/tmp/patches_segmentation.png")
         Image.fromarray(np.reshape((patches_gen_image[20] + 1.0) / 2.0 * 255.0, (patches_gen_image.shape[1], patches_gen_image.shape[2], 3)).astype(np.uint8)).save("/tmp/patches_gen_image.png")
 
+        patch_count = patches_image.shape[0]
+        split_count = 5
+        batch_size = int(math.ceil(patch_count / split_count))
+
+        assert patches_image.shape[0] == patches_segmentation.shape[0] and patches_segmentation.shape[0] == patches_gen_image.shape[0]
+
         print("Starting training...")
         for epoch in range(3000):
-            _, cost_disc = sess.run([update_disc, loss_disc], feed_dict={image_patches: patches_image, image: patches_gen_image, mask_patches: patches_segmentation})
-            _, cost_gen = sess.run([update_gen, loss_gen], feed_dict={image: patches_gen_image, image_patches: patches_image})
+            cost_disc = 0
+            for i in range(split_count):
+                offset = i * batch_size
+                count = batch_size if offset + batch_size <= patch_count else patch_count - offset
+                _, cost_disc2 = sess.run([update_disc, loss_disc], feed_dict={
+                    image_patches: patches_image[offset:offset+count],
+                    image: patches_gen_image[offset:offset+count],
+                    mask_patches: patches_segmentation[offset:offset+count]})
+                cost_disc += cost_disc2
+
+            cost_gen = 0
+            for i in range(split_count):
+                offset = i * batch_size
+                count = batch_size if offset + batch_size <= patch_count else patch_count - offset
+                _, cost_gen2 = sess.run([update_gen, loss_gen], feed_dict={
+                    image: patches_gen_image[offset:offset+count],
+                    mask_patches: patches_segmentation[offset:offset+count],
+                    image_patches: patches_image[offset:offset+count]})
+
+                cost_gen += cost_gen2
 
             if epoch % 1 == 0:
                 print(f"{epoch} losses: gen={cost_gen:.8f}, disc={cost_disc:.8f}")
-
-            # if epoch % 1 == 0:
-            #     sample = sess.run(gen_net_use, feed_dict={image: patches_gen_image})
-            #
-            #     Image.fromarray(np.reshape((sample + 1.0) / 2.0 * 255.0, (sample.shape[1], sample.shape[2])).astype(np.uint8)).save(f"/tmp/res_{epoch}.png")
-            #     print("Saved image")
 
             if epoch % 10 == 0:
                 sample = sess.run(gen_net, feed_dict={image: patches_gen_image})
