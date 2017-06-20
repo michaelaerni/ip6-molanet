6abf3cdf5465b0bd30857f4520d6b8fd820d17ba
Patrick Del Conte <p.del.conte@hotmail.ch>
2017-06-20 14:31:27 +0200

    removed local path

--- molanet/data/load_dermofit.py
+++ molanet/data/load_dermofit.py
@@ -79,7 +79,7 @@ def create_arg_parser() -> argparse.ArgumentParser:
                         help="Name of the data source stored in the database")
     parser.add_argument("--lesionlist", type=str, default="lesionlist.txt",
                         help="name of the file with the lesiondescriptions")
-    parser.add_argument("--lesiondir", type=str, default=r"C:\Users\pdcwi\Documents\IP6 nonsynced\dermofit",
+    parser.add_argument("--lesiondir", type=str, default=None,
                         help=" Path to the folder containing the lesion folders and lesionlist ")
     parser.add_argument("--offset", type=int, default=0, help="Starting offset in data set")
 
