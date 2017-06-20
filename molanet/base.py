7d9867fea04426545f25d541051cb0401fb69f42
Michael Aerni <michael.aerni@hotmail.com>
2017-06-20 18:25:23 +0200

    Created interface for loss function factories

--- molanet/base.py
+++ molanet/base.py
@@ -21,7 +21,7 @@ class NetworkFactory(object):
 
         :param source_tensor: Input for the created generator
         :param reuse: If False, the weights cannot exist yet, if True they will be reused. Defaults to False.
-        :param apply_summary: If True, summary operations will be added to the network. Defaults to True.
+        :param apply_summary: If True, summary operations will be added to the graph. Defaults to True.
         :return: Output tensor of the created generator
         """
 
@@ -40,8 +40,60 @@ class NetworkFactory(object):
         :param source_tensor: Input tensor for the corresponding generator
         :param target_tensor: Tensor of the generated value for the input source_tensor
         :param reuse: If False, the weights cannot exist yet, if True they will be reused. Defaults to False.
-        :param apply_summary: If True, summary operations will be added to the network. Defaults to True.
+        :param apply_summary: If True, summary operations will be added to the graph. Defaults to True.
         :return: Output tensor of the created discriminator as unscaled logits
+        """
+
+        raise NotImplementedError("This method should be overridden by child classes")
+
+
+class ObjectiveFactory(object):
+    """
+    Factory for generator and discriminator objectives (cost functions).
+
+    This is an abstract base class.
+    Child classes should inherit override the objective creation methods.
+    Additional network configuration should be supplied in the child's constructor.
+    """
+
+    def create_generator_loss(
+            self,
+            source_tensor: tf.Tensor,
+            target_tensor: tf.Tensor,
+            generator: tf.Tensor,
+            generator_discriminator: tf.Tensor,
+            apply_summary: bool = True
+    ) -> tf.Tensor:
+        """
+        Creates the generator loss function and optionally applies summary options.
+
+        :param source_tensor: Input tensor which is fed to the generator
+        :param target_tensor: Ground truth output for the given source_tensor
+        :param generator: Generated output for the given source_tensor
+        :param generator_discriminator: Discriminator logits for the generated output corresponding to the source_tensor
+        :param apply_summary: If True, summary operations will be added to the graph. Defaults to True.
+        :return: Loss function for the generator which can be used for optimization
+        """
+
+        raise NotImplementedError("This method should be overridden by child classes")
+
+    def create_discriminator_loss(
+            self,
+            source_tensor: tf.Tensor,
+            target_tensor: tf.Tensor,
+            generator_discriminator: tf.Tensor,
+            real_discriminator: tf.Tensor,
+            apply_summary: bool = True
+    ) -> tf.Tensor:
+        """
+        Creates the discriminator loss function and optionally applies summary options.
+
+        :param source_tensor: Input tensor which is fed to the generator
+        :param target_tensor: Ground truth output for the given source_tensor
+        :param generator_discriminator: Discriminator logits for the generated output corresponding to the source_tensor
+        :param real_discriminator: Discriminator logits for the ground truth output
+        :param apply_summary: If True, summary operations will be added to the graph. Defaults to True.
+        :return: Loss function for the discriminator which can be used for optimization
         """
 
         raise NotImplementedError("This method should be overridden by child classes")
