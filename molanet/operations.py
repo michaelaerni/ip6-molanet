e1d687573a65e23aa50d33f290da7a97e9d59645
Michael Aerni <michael.aerni@hotmail.com>
2017-08-05 17:20:41 +0200

    Created global conv2d and conv2d_dilated functions

--- molanet/operations.py
+++ molanet/operations.py
@@ -1,4 +1,4 @@
-from typing import Callable
+from typing import Callable, Tuple
 
 import tensorflow as tf
 
@@ -22,3 +22,96 @@ def select_device(use_gpu: bool) -> Callable[[tf.Operation], str]:
 
 def use_cpu():
     return tf.device("/cpu:0")
+
+
+def conv2d_dilated(
+        input_tensor: tf.Tensor,
+        feature_count: int,
+        dilation_rate: int,
+        name: str,
+        filter_size: int = 3,
+        data_format: str = "NHWC"
+) -> Tuple[tf.Tensor, tf.Tensor, tf.Tensor]:
+
+    with tf.name_scope(f"conv2d_dilated_{name}"):
+
+        # Check data format and select input feature count
+        tf.assert_rank(input_tensor, 4)
+        input_shape = input_tensor.get_shape()
+        if data_format == "NHWC":
+            input_feature_count = input_shape[-1]
+        elif data_format == "NCHW":
+            input_feature_count = input_shape[1]
+        else:
+            raise ValueError(f"Unsupported data format {data_format}")
+
+        # Create variables
+        w = tf.get_variable(
+            f"w_{name}",
+            shape=[filter_size, filter_size, input_feature_count, feature_count],
+            dtype=tf.float32,
+            initializer=tf.uniform_unit_scaling_initializer(1.43))  # TODO: Use correct initializer
+        b = tf.get_variable(f"b_{name}",
+                            shape=[feature_count], dtype=tf.float32, initializer=tf.constant_initializer(0.0))
+
+        # Perform convolution
+        conv = tf.nn.convolution(
+            input_tensor,
+            filter=w,
+            padding="SAME",  # TODO: Make padding configurable
+            dilation_rate=[dilation_rate, dilation_rate],
+            data_format=data_format)
+
+        result = tf.nn.bias_add(conv, b, data_format=data_format)
+        result = tf.nn.relu(result)
+
+        return result, w, b
+
+
+def conv2d(
+        input_tensor: tf.Tensor,
+        feature_count: int,
+        name: str,
+        filter_size: int,
+        stride: int = 1,
+        do_batchnorm: bool = True,
+        do_activation: bool = True,
+        data_format: str = "NHWC"
+) -> Tuple[tf.Tensor, tf.Tensor, tf.Tensor]:
+
+    with tf.name_scope(f"conv2d_{name}"):
+        # Check data format and select input feature count
+        tf.assert_rank(input_tensor, 4)
+        input_shape = input_tensor.get_shape()
+        if data_format == "NHWC":
+            input_feature_count = input_shape[-1]
+        elif data_format == "NCHW":
+            input_feature_count = input_shape[1]
+        else:
+            raise ValueError(f"Unsupported data format {data_format}")
+
+        # Create variables
+        w = tf.get_variable(
+            f"w_{name}",
+            shape=[filter_size, filter_size, input_feature_count, feature_count],
+            dtype=tf.float32,
+            initializer=tf.truncated_normal_initializer(stddev=0.02))  # TODO: Use correct initializer
+        b = tf.get_variable(f"b_{name}",
+                            shape=[feature_count], dtype=tf.float32, initializer=tf.constant_initializer(0.0))
+
+        conv = tf.nn.conv2d(
+            input_tensor,
+            filter=w,
+            strides=[1, stride, stride, 1],
+            padding="SAME",
+            data_format=data_format)  # TODO: Make padding configurable
+
+        result = tf.nn.bias_add(conv, b, data_format=data_format)
+
+        if do_batchnorm:
+            result = tf.contrib.layers.batch_norm(result, decay=0.9, epsilon=1e-5, fused=True)  # TODO: Params?
+
+        if do_activation:
+            result = leaky_relu(result, 0.2)
+
+        return result, w, b
