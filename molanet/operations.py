d3df6b74008fcedf26537406039021ce695883b1
Michael Aerni <michael.aerni@hotmail.com>
2017-08-12 11:44:42 +0200

    Made weight initializer configurable in operations

--- molanet/operations.py
+++ molanet/operations.py
@@ -63,7 +63,8 @@ def conv2d_dilated(
         dilation_rate: int,
         name: str,
         filter_size: int = 3,
-        data_format: str = "NHWC"
+        data_format: str = "NHWC",
+        weight_initializer: tf.Initializer = tf.uniform_unit_scaling_initializer(1.43)
 ) -> Tuple[tf.Tensor, tf.Tensor, tf.Tensor]:
 
     with tf.name_scope(f"conv2d_dilated_{name}"):
@@ -83,7 +84,7 @@ def conv2d_dilated(
             f"w_{name}",
             shape=[filter_size, filter_size, input_feature_count, feature_count],
             dtype=tf.float32,
-            initializer=tf.uniform_unit_scaling_initializer(1.43))  # TODO: Use correct initializer
+            initializer=weight_initializer)
         b = tf.get_variable(f"b_{name}",
                             shape=[feature_count], dtype=tf.float32, initializer=tf.constant_initializer(0.0))
 
@@ -112,7 +113,8 @@ def conv2d_transpose(
         stride: int = 1,
         do_batchnorm: bool = True,
         do_activation: bool = True,
-        data_format: str = "NHWC"
+        data_format: str = "NHWC",
+        weight_initializer: tf.Initializer = tf.uniform_unit_scaling_initializer(1.43)
 ) -> Tuple[tf.Tensor, tf.Tensor, tf.Tensor]:
 
     with tf.name_scope(f"conv2d_transpose_{name}"):
@@ -139,7 +141,7 @@ def conv2d_transpose(
             f"w_{name}",
             shape=[filter_size, filter_size, feature_count, input_feature_count],
             dtype=tf.float32,
-            initializer=tf.uniform_unit_scaling_initializer(1.43))  # TODO: Use correct initializer
+            initializer=weight_initializer)
         b = tf.get_variable(f"b_{name}",
                             shape=[feature_count], dtype=tf.float32, initializer=tf.constant_initializer(0.0))
 
@@ -177,7 +179,8 @@ def conv2d(
         stride: int = 1,
         do_batchnorm: bool = True,
         do_activation: bool = True,
-        data_format: str = "NHWC"
+        data_format: str = "NHWC",
+        weight_initializer: tf.Initializer = tf.truncated_normal_initializer(stddev=0.02)
 ) -> Tuple[tf.Tensor, tf.Tensor, tf.Tensor]:
 
     with tf.name_scope(f"conv2d_{name}"):
@@ -198,7 +201,7 @@ def conv2d(
             f"w_{name}",
             shape=[filter_size, filter_size, input_feature_count, feature_count],
             dtype=tf.float32,
-            initializer=tf.truncated_normal_initializer(stddev=0.02))  # TODO: Use correct initializer
+            initializer=weight_initializer)
         b = tf.get_variable(f"b_{name}",
                             shape=[feature_count], dtype=tf.float32, initializer=tf.constant_initializer(0.0))
 
@@ -218,76 +221,3 @@ def conv2d(
             result = leaky_relu(result, 0.2)
 
         return result, w, b
-
-
-def resize_conv2d(input_tensor: tf.Tensor,
-                  feature_count: int,
-                  name: str,
-                  filter_size: int,
-                  output_shape_2d: Tuple[tf.Tensor, tf.Tensor],
-                  keep_probability: tf.Tensor = None,
-                  concat_activations: tf.Tensor = None,
-                  do_batchnorm: bool = True,
-                  do_activation: bool = True,
-                  data_format: str = "NHWC"
-                  ) -> Tuple[tf.Tensor, tf.Tensor, tf.Tensor]:
-    # Check data format and select input feature count
-
-    tf.assert_rank(input_tensor, 4)
-    if data_format == "NHWC":
-        concat_axis = 3
-        resized = tf.image.resize_nearest_neighbor(input_tensor, (output_shape_2d[1], output_shape_2d[0]),
-                                                   name=f"{name}_resize")
-    elif data_format == "NCHW":
-        concat_axis = 1
-        resized = tf.image.resize_nearest_neighbor(input_tensor, (output_shape_2d[0], output_shape_2d[1]),
-                                                   name=f"{name}_resize")
-    else:
-        raise ValueError(f"Unsupported data format {data_format}")
-
-    """
-    this method assumes the features are of shape [B
-
-    See
-    https://distill.pub/2016/deconv-checkerboard/
-    Our experience has been that nearest-neighbor resize followed by a convolution works very well, in a wide variety of contexts.
-
-    tf.image.resize_images()
-    tf.pad()
-    tf.nn.conv2d()
-    """
-    # TODO resize_images distorts dimensions
-    # TODO fix by using tf.image.resize_image_with_crop_or_pad() but we NEED nearest neighbour (default is Bilinear)
-
-    # scale does not preserve aspect ration but NxN images should be fine
-
-    # padding = tf.constant(output_size - (resized.get_shape().as_list()[1] * 2 - filter_size + 1))
-    # resized_padded = tf.cond(padding > 0, lambda: tf.image.pad_to_bounding_box(resized, padding, padding,
-    #                                                                            output_size +2*padding,
-    #                                                                            output_size +2*padding),
-    #                          lambda: resized)
-
-    conv, w, b = conv2d(resized,
-                        feature_count,
-                        f"{name}_conv2d", filter_size,
-                        stride=1,
-                        do_batchnorm=do_batchnorm,
-                        do_activation=do_activation,
-                        data_format=data_format)
-
-    if do_batchnorm:
-        bn = tf.contrib.layers.batch_norm(conv, decay=0.9, epsilon=1e-5, fused=True)  # TODO: Params?
-    else:
-        bn = conv
-    d = tf.nn.dropout(bn, keep_probability)
-
-    if do_activation:
-        a = tf.nn.relu(d)
-    else:
-        a = d
-
-    # Concat activations if available
-    if concat_activations is not None:
-        a = tf.concat([a, concat_activations], axis=concat_axis)
-
-    return a, w, b
